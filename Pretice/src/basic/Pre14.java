ff83e793f3d0c4aaf5c9a82ff7e5fa718f56267a
kwonjihoo <yolohoo@naver.com>
2023-08-28 17:36:56 +0900

    오후 수업 완료

--- Pretice/src/basic/Pre14.java
+++ Pretice/src/basic/Pre14.java
@@ -11,15 +11,16 @@ public class Pre14 {
 		 */
 		
 		int[][] score = {
-				{79, 80, 99}, //A학생
-				{95, 85, 89}, //B학생
-				{90, 65, 56}, //C학생
-				{69, 78, 77} //D학생
+				{79, 80, 99}, //A학생 0,1
+				{95, 85, 89}, //B학생 1,1 
+				{90, 65, 56}, //C학생 2, 1
+				{69, 78, 77} //D학생  3,1
 				//과목: 3과목
 		};
 		String[] stuName = {"A학생", "B학생", "C학생", "D학생"};
 		String[] subName = {"국어", "영어", "수학"};
 		//1
+		double total2 = 0.0;
 		int idx =0;
 		for(int[]s : score) {
 			//배열하나하나를 가져옴
@@ -30,11 +31,29 @@ public class Pre14 {
 				
 			}
 			double avg = (double)total / subName.length;
+			total2 += avg; //각 평균들의 합
 			//total과 subName은 int와 String타입이기에 double타입으로..
 			System.out.printf("%s의 평균: %.1f점 \n", stuName[idx], avg);
 			idx++; // 0,1,2 등으로 증가하며 배열의 값을 차례대로 불러온다. 
 		}
 		System.out.println("====================");
+		// 2. 각 과목의 평균을 출력해 보세요.
+		
+		
+		for (int i =0; i<3; i++) {
+			int total =0;
+			for(int j =0; j<4; j++) {
+				total += score[j][i]; //각 과목의 합
+			}
+			double avg = (double)total / stuName.length;
+			System.out.printf("%s의 평균: %.1f점 \n" ,subName[i], avg);
+		}
+		System.out.println("====================");
+		
+		
+		//3. 반 평균을 출력해 보세요. (모든 학생들의 평균의 평균)
+		double classAvg = total2 / stuName.length;
+		System.out.printf("반 평균 : %.1f점", classAvg);
 	}
 
 }
